a2bf0270afe8d5d17b01ace0ed6fc8f439f02f60
Michal <lonaro96@gmail.com>
2018-11-17 15:07:18 +0100

    Repair recyclerview in TemplateFragment.

--- app/src/main/java/michal/cardmaker/presenter/TemplateAdapter.java
+++ app/src/main/java/michal/cardmaker/presenter/TemplateAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import michal.cardmaker.R;
 import michal.cardmaker.view.TemplateSinglePhoto;
 
 public class TemplateAdapter extends RecyclerView.Adapter<TemplateViewHolder> {
@@ -25,7 +27,7 @@ public class TemplateAdapter extends RecyclerView.Adapter<TemplateViewHolder> {
     @Override
     public TemplateViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
-        View imageView = new ImageView(viewGroup.getContext());//LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_template_item, viewGroup, false);
+        View imageView = LayoutInflater.from(context).inflate(R.layout.recyclerview_template_item, viewGroup, false);
 
         return new TemplateViewHolder(imageView);
     }
--- app/src/main/java/michal/cardmaker/presenter/TemplateViewHolder.java
+++ app/src/main/java/michal/cardmaker/presenter/TemplateViewHolder.java
@@ -4,12 +4,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import michal.cardmaker.R;
+
 public class TemplateViewHolder extends RecyclerView.ViewHolder {
 
     ImageView imageView;
 
     public TemplateViewHolder(View imageView) {
         super(imageView);
-        this.imageView = (ImageView) imageView;
+        this.imageView = imageView.findViewById(R.id.template_item_image);
     }
 }
